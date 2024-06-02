92032bb1ce228c29b57dc59bc49c267d5b1d503d
yazeedalzahrani <92376728+yazeedalzahrani@users.noreply.github.com>
2024-06-03 00:18:51 +0100

    d

--- webflow.43bb934f9.js
+++ webflow.43bb934f9.js
@@ -1,10 +1,3 @@
-/*!
- * Webflow: Front-end site library
- * @license MIT
- * Inline scripts may access the api using an async handler:
- *   var Webflow = Webflow || [];
- *   Webflow.push(readyFunction);
- */
 
 (() => {
     var __create = Object.create;
@@ -40,10 +33,7 @@
         return to;
     };
     var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
-        // If the importer is in node compatibility mode or this is not an ESM
-        // file that has been converted to a CommonJS file using a Babel-
-        // compatible transform (i.e. "__esModule" has not been set), then set
-        // "default" to the CommonJS "module.exports" for node compatibility.
+     
         isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", {
             value: mod,
             enumerable: true
